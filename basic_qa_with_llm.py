e50b3799c1eb8b29ef70859e174998089202457f
Manish Bhobe <mjbhobe@gmail.com>
2025-04-24 11:00:24 +0530

    New Code examples

--- basic_qa_with_llm.py
+++ basic_qa_with_llm.py
@@ -8,15 +8,13 @@ Code is meant for learning purposes ONLY!
 """
 
 from dotenv import load_dotenv
-from rich import print
 from rich.console import Console
 from rich.markdown import Markdown
 
-# from llama_index.core.base.llms.types import ChatMessage
 from llama_index.llms.groq import Groq
 
 # load your API keys from .env file
-# It should have entry for GROQ_API_KEY
+# It should have entry for GROQ_API_KEY (create a new one at https://console.groq.com/keys)
 load_dotenv()
 console = Console()
 
@@ -28,26 +26,26 @@ llm = Groq(
 )
 
 
-def get_completion(part: str) -> str:
-    response = llm.complete(part)
+def get_completion(prompt: str) -> str:
+    """utility function to pass a prompt to LLM & get a response
+    Args:
+        prompt (str) - the query to your LLM
+    Returns:
+        response (str) - text response from LLM (response.content)
+    """
+    response = llm.complete(prompt)
     return response.text
 
 
 # now ask the LLM to complete the text
 
-# text = ""
-# while True:
-#     # continuous loop, broken when user enters any one of exit|bye|quit
-#     text = input("Enter completion (type exit or quit or bye to quit): ")
-#     if text.strip().lower() in ["exit", "quit", "bye"]:
-#         break
-#     print(f"[{text}] {get_completion(text)}")
-
 user_prompt = None
 while True:
-    # infinite loop
+    # infinite loop, broken when user types any of ["bye", "quit", "exit"]
     console.print("[cyan]Ask me anything [/cyan][yellow](type exit to quit)[/yellow]: ")
     user_prompt = console.input("[green]").strip()
     if user_prompt.strip().lower() in ["bye", "quit", "exit"]:
         break
-    console.print(Markdown(get_completion(user_prompt)))
+    response = llm.complete(user_prompt)
+    console.print(Markdown(response.text))
+    # console.print(Markdown(get_completion(user_prompt)))
